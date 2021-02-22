3bbbeb7b5dfa87ded586869c9eeae0b182c80783
emilthedev <emilcode2@gmail.com>
2021-02-22 18:01:33 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# elance
+Freelancer landing page clone
+
+Made as a practice of frontend development
---
+++ js/index.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $(".how-it-works").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("project-btn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function justDemo(){
+  alert("The button works!")
+}
